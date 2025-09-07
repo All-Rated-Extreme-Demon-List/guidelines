14a7213fe1a5c822568dc20d0806dfaee5f848ca
sphericle <jackb2033@gmail.com>
2025-09-07 01:12:11 -0400

    changes 3

--- build.js
+++ build.js
@@ -6,28 +6,31 @@ const guidelinesPath = "./";
 
 const ignored = fs.readFileSync("./.gitignore", "utf-8").split("\n");
 
-let content = "";
+const files = []
+
 
 fs.readdirSync(guidelinesPath).forEach(file => {
+    let fileStr = "";
     if (file.startsWith(".")) return;
     if (ignored.includes(file)) return;
     if (!fs.statSync(file).isDirectory()) return;
 
     const indexFileResponse = fs.readFileSync(`${guidelinesPath}${file}/index.md`, "utf-8");
-    content += indexFileResponse.trim() + "\n\n";
+    fileStr += indexFileResponse.trim() + "\n\n";
     fs.readdirSync(`${guidelinesPath}${file}`).forEach(subfile => {
         if (subfile == "index.md") return;
         if (ignored.includes(subfile)) return;
         if (fs.statSync(`${guidelinesPath}${file}/${subfile}`).isDirectory()) return;
         
         const subfileContent = fs.readFileSync(`${guidelinesPath}${file}/${subfile}`, "utf-8");
-        content += subfileContent.trim() + "\n\n";
+        fileStr += subfileContent.trim() + "\n\n";
     });
+    files.push(fileStr);
 
 
 })
 
-fs.writeFileSync("./data.txt", content);
+fs.writeFileSync("./data.json", JSON.stringify(files));
 
 console.log("Guidelines data building done.")
 
